6503fea3d67ff469c140562e1e1294b97151919e
thom lake <thom.l.lake@gmail.com>
2014-04-21 19:48:44 -0400

    fixed typo in sparsity gradient

---
+++ data/makedata.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+
+def dump_sentences():
+    corpus = fetch_20newsgroups(subset='train')
+    docs = corpus.data
+    labels = corpus.target
+    label_names = corpus.target_names
+    vectorizer = CountVectorizer(token_pattern=r'(?u)\b\w+\b')
+    preprocess = vectorizer.build_preprocessor()
+    tokenize = vectorizer.build_tokenizer()
+    
+    def words(doc):
+        p = preprocess(doc)
+        return ' '.join(t.encode('ascii', 'replace') for t in tokenize(p))
+        
+    with open('20news.txt', 'w') as f:
+        for doc, lbl in zip(docs, labels):
+            print >> f, label_names[lbl]
+            print >> f, words(doc)
+
+if __name__ == '__main__':
+    dump_sentences()
+
